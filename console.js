a6873428f61770e8ea40fa640048ba81ba4a44a4
Ednei Andres <ediedneiwilliam@gmail.com>
2023-03-21 21:24:23 -0300

    Criado arquivo explicando Console e Ternario

---
+++ console.js
@@ -0,0 +1,7 @@
+// log -> registro
+const minhaVariavel = true;
+console.log(245)
+console.log("eu sou um texto")
+console.log(minhaVariavel)
+
+console.error(2 / 0)
---
+++ ternario.js
@@ -0,0 +1,12 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+// if (idadeCliente >= idadeMinima) {
+//     console.log("Servir cerveja")
+// } else {
+//     console.log("Servir suco de laranja natural com adoçante")
+// }
+
+console.log(idadeCliente >= idadeMinima ? "Servir cerveja" : "Servir suco");
+// [COMPARACAO] ?_TERNARIO TRUE : FALSE. Tipo procv
+// Operador ternário tem a desvantagem de ser mais difícil de ler 
