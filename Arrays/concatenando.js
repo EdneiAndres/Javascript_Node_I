96edaf1449b84d4c0abd7a3ef67f84a4b574cd0e
Ednei Andres <ediedneiwilliam@gmail.com>
2023-03-24 01:05:44 -0300

    Estudando arrays em mais de uma dimensão

---
+++ Arrays/concatenando.js
@@ -0,0 +1,6 @@
+const salaJs = ["Evanilsson", "Tiago", "Jorcelino", "Samara"]
+const salaPython = ["Ednei", "Samuca", "Dhyego", "Raquel"]
+
+const todosJuntos = salaJs.concat(salaPython);
+
+console.log(todosJuntos)
---
+++ Arrays/lista-duas-dimensoes.js
@@ -0,0 +1,11 @@
+const alunos = ['João', 'Juliana', 'Ana', 'Caio'];
+const medias = ['10, 8, 7.5, 9'];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+console.log(listaDeAlunosEMedias);
+// Acessando um valor em uma lista dentro de una lista
+console.log(listaDeAlunosEMedias[0][1])
+
+const teste = ['Andre', 'Antônio']
+
